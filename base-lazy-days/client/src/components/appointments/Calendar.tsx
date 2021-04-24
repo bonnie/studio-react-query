9e8aa820528abb19d39918cc863ded35d60e571d
Bonnie Schulkin <biz@bonnieschulkin.com>
2021-04-23 18:29:04 -0700

    Stying updates

--- base-lazy-days/client/src/components/appointments/Calendar.tsx
+++ base-lazy-days/client/src/components/appointments/Calendar.tsx
@@ -51,13 +51,13 @@ export function Calendar(): ReactElement {
   }
   return (
     <Box>
-      <HStack m={5} spacing={8} justify="center">
+      <HStack mt={10} spacing={8} justify="center">
         <IconButton
           aria-label="previous month"
           onClick={() => updateMonth(-1)}
           icon={<TiArrowLeftThick />}
         />
-        <Heading size="2xl" minW="40%" textAlign="center">
+        <Heading minW="40%" textAlign="center">
           {monthData.monthName} {monthData.year}
         </Heading>
         <IconButton
@@ -76,7 +76,7 @@ export function Calendar(): ReactElement {
           Only show available
         </Checkbox>
       </HStack>
-      <Grid templateColumns="repeat(7, 1fr)" gap={4} m={10}>
+      <Grid templateColumns="repeat(7, 1fr)" gap={4} my={5} mx={10}>
         {/* first day needs a grid column */}
         <DateBox
           date={1}
--- base-lazy-days/client/src/components/staff/AllStaff.tsx
+++ base-lazy-days/client/src/components/staff/AllStaff.tsx
@@ -1,17 +1,37 @@
-import { HStack } from '@chakra-ui/react';
-import { ReactElement } from 'react';
+import { Box, Heading, HStack, Radio, RadioGroup } from '@chakra-ui/react';
+import { ReactElement, useState } from 'react';
 
+import { useTreatments } from '../treatments/useTreatments';
 import { Staff } from './Staff';
 import { useStaff } from './useStaff';
 
 export function AllStaff(): ReactElement {
+  // for filtering staff by treatment
+  const [filter, setFilter] = useState('all');
+
   // replace with data from React Query
   const { staff } = useStaff();
+  const { treatments } = useTreatments();
+
   return (
-    <HStack m={10} spacing={8} justify="center">
-      {staff.map((staffData) => (
-        <Staff key={staffData.id} staffData={staffData} />
-      ))}
-    </HStack>
+    <Box>
+      <Heading mt={10} align="center">
+        Our Staff
+      </Heading>
+      <HStack m={10} spacing={8} justify="center">
+        {staff.map((staffData) => (
+          <Staff key={staffData.id} staffData={staffData} />
+        ))}
+      </HStack>
+      <RadioGroup onChange={setFilter} value={filter}>
+        <HStack my={10} spacing={8} justify="center">
+          <Heading size="md">Filter by treatment:</Heading>
+          <Radio value="all">All</Radio>
+          {treatments.map((t) => (
+            <Radio value={t.name}>{t.name}</Radio>
+          ))}
+        </HStack>
+      </RadioGroup>
+    </Box>
   );
 }
