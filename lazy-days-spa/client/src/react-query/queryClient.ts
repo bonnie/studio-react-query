959c7340a8233e6b07dca4618362f58b65c1c0d0
Bonnie Schulkin <biz@bonnieschulkin.com>
2021-04-29 09:51:12 -0700

    Add code to prevent duplicate toasts

--- lazy-days-spa/client/src/react-query/queryClient.ts
+++ lazy-days-spa/client/src/react-query/queryClient.ts
@@ -4,13 +4,17 @@ import { QueryClient } from 'react-query';
 const toast = createStandaloneToast();
 
 export function queryErrorHandler(error: unknown): void {
-  // error is unknown because in js, anything can be an error (e.g. throw(5))
+  // error is type unknown because in js, anything can be an error (e.g. throw(5))
+  const id = 'react-query-error';
   const title =
     error instanceof Error
       ? // remove the initial 'Error: ' that accompanies many errors
         error.toString().replace(/^Error:\s*/, '')
       : 'error connecting to server';
-  toast({ title, status: 'error', variant: 'subtle', isClosable: true });
+
+  // prevent duplicate toasts
+  if (!toast.isActive(id))
+    toast({ id, title, status: 'error', variant: 'subtle', isClosable: true });
 }
 
 export const queryClient = new QueryClient({
