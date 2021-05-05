7786aa4ac2c9f03598e580bd590073727fd11a2f
Bonnie Schulkin <biz@bonnieschulkin.com>
2021-05-04 17:11:30 -0700

    Add formik for user profile

--- lazy-days-spa/client/src/components/user/UserProfile.tsx
+++ lazy-days-spa/client/src/components/user/UserProfile.tsx
@@ -4,16 +4,15 @@ import {
   Button,
   Flex,
   FormControl,
-  FormErrorMessage,
   FormLabel,
   Heading,
   Input,
   Stack,
 } from '@chakra-ui/react';
-import { ReactElement, useState } from 'react';
+import { Field, Form, Formik } from 'formik';
+import { ReactElement } from 'react';
 import { Redirect } from 'react-router-dom';
 
-import type { User } from '../../../../shared/types';
 import { usePatchUser } from './hooks/usePatchUser';
 import { useUser } from './hooks/useUser';
 import { UserAppointments } from './UserAppointments';
@@ -22,32 +21,16 @@ export function UserProfile(): ReactElement {
   const { user } = useUser();
   const patchUser = usePatchUser();
 
-  // a shell user that will allow us to type formData as User
-  // and avoid "uncontrolled to controlled field" errors
-  const emptyUser = {
-    id: -1,
-    email: 'none@none.none',
-    name: '',
-    address: '',
-    phone: '',
-  };
-
   const formElements = ['name', 'address', 'phone'];
-  const [formData, setFormData] = useState<User>(
-    { ...emptyUser, ...user } || emptyUser,
-  );
-  const [dirty, setDirty] = useState({ email: false });
 
   if (!user) {
     return <Redirect to="/signin" />;
   }
 
-  function updateForm(fieldName: string, value: string) {
-    setFormData((prevData) => {
-      const newData = { ...prevData };
-      newData[fieldName] = value;
-      return newData;
-    });
+  interface FormValues {
+    name: string;
+    address: string;
+    phone: string;
   }
 
   return (
@@ -58,35 +41,27 @@ export function UserProfile(): ReactElement {
           <Heading>Your information</Heading>
         </Stack>
         <Box rounded="lg" bg="white" boxShadow="lg" p={8}>
-          <Stack spacing={4}>
-            <FormControl
-              id="email"
-              isRequired
-              isInvalid={!formData?.email && dirty.email}
-            >
-              <FormLabel>Email address</FormLabel>
-              <Input
-                type="email"
-                value={formData?.email}
-                onChange={(e) => updateForm('email', e.target.value)}
-                onBlur={() =>
-                  setDirty((prevDirty) => ({ ...prevDirty, email: true }))
-                }
-              />
-              <FormErrorMessage>Email may not be blank</FormErrorMessage>
-            </FormControl>
-            {formElements.map((element) => (
-              <FormControl key={element} id={element}>
-                <FormLabel>{element}</FormLabel>
-                <Input
-                  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-                  value={formData[element]}
-                  onChange={(e) => updateForm(element, e.target.value)}
-                />
-              </FormControl>
-            ))}
-            <Button onClick={() => patchUser(formData)}>Update</Button>
-          </Stack>
+          <Formik
+            initialValues={{
+              email: user.email,
+              name: user.name ?? '',
+              address: user.address ?? '',
+              phone: user.phone ?? '',
+            }}
+            onSubmit={(values: FormValues) => {
+              patchUser({ ...user, ...values });
+            }}
+          >
+            <Form>
+              {formElements.map((element) => (
+                <FormControl key={element} id={element}>
+                  <FormLabel>{element}</FormLabel>
+                  <Field name={element} as={Input} />
+                </FormControl>
+              ))}
+              <Button type="submit">Update</Button>
+            </Form>
+          </Formik>
         </Box>
       </Stack>
     </Flex>
--- lazy-days-spa/client/src/components/user/hooks/usePatchUser.ts
+++ lazy-days-spa/client/src/components/user/hooks/usePatchUser.ts
@@ -1,86 +1,42 @@
-import axios, { AxiosResponse } from 'axios';
 import jsonpatch from 'fast-json-patch';
-import { UseMutateFunction, useMutation, useQueryClient } from 'react-query';
+import { UseMutateFunction, useMutation } from 'react-query';
 
 import type { User } from '../../../../../shared/types';
 import { axiosInstance, getJWTHeader } from '../../../axiosInstance';
-import { queryKeys } from '../../../react-query/constants';
 import { useUser } from './useUser';
-
-interface AxiosResponseWithCancel extends AxiosResponse {
-  cancel: () => void;
-}
 
 async function patchUserOnServer(
   originalData: User | null,
   newData: User | null,
-): Promise<AxiosResponseWithCancel | null> {
+): Promise<User | null> {
   if (!newData || !originalData) return null;
-
   // create a patch for the difference between newData and originalData
   const patch = jsonpatch.compare(originalData, newData);
 
-  // Create a new CancelToken source for this request
-  const source = axios.CancelToken.source();
-
-  // send patched data to the server with cancel token and return the promise
-  const promise: AxiosResponseWithCancel = await axiosInstance.patch(
+  // send patched data to the server
+  const { data } = await axiosInstance.patch(
     `/user/${originalData.id}`,
     { patch },
     {
       headers: getJWTHeader(originalData),
-      cancelToken: source.token,
     },
   );
-
-  // Cancel the request if React Query calls the `promise.cancel` method
-  promise.cancel = () => {
-    source.cancel('Query was cancelled by React Query');
-  };
-
-  return promise;
+  return data.user;
 }
 
 export function usePatchUser(): UseMutateFunction<
-  AxiosResponseWithCancel | null,
+  User | null,
   unknown,
   User | null,
-  {
-    previousUser: User | null | undefined;
-  }
+  unknown
 > {
   const { user, updateUser } = useUser();
-  const queryClient = useQueryClient();
 
   const { mutate: patchUser } = useMutation(
     (newData: User | null) => patchUserOnServer(user, newData),
     {
-      onMutate: async (newData: User | null) => {
-        // Cancel any outgoing refetches (so they don't overwrite our optimistic update)
-        await queryClient.cancelQueries(queryKeys.user);
-
-        // Snapshot the previous value
-        const previousUser: User | null | undefined = queryClient.getQueryData(
-          queryKeys.user,
-        );
-
-        // Optimistically update to the new value
-        if (newData) updateUser(newData);
-
-        // Return a context object with the snapshotted value
-        return { previousUser };
-      },
-      // If the mutation fails, use the context returned from onMutate to roll back
-
-      onError: (err, newTodo, context) => {
-        console.log('YOU FAILED, YA LOSER!!!');
-        console.log('setting user back to', context?.previousUser);
-        queryClient.setQueryData(queryKeys.user, context?.previousUser);
-      },
-
-      // Always refetch after error or success:
-      onSettled: () => {
-        queryClient.invalidateQueries(queryKeys.user);
+      onSuccess: (userData: User | null) => {
+        if (userData) updateUser(userData);
       },
     },
   );
