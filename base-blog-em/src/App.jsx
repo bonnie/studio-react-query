85fb62ee773f5102e004579895081677808132ef
Bonnie Schulkin <biz@bonnieschulkin.com>
2021-05-11 11:04:52 -0700

    Finished project

--- base-blog-em/src/App.jsx
+++ base-blog-em/src/App.jsx
@@ -1,14 +1,22 @@
 import "./App.css";
 
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
 import { Posts } from "./Posts";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     // provide React Query client to App
-    <div className="App">
-      <h1>Blog &apos;em Ipsum</h1>
-      <Posts />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <h1>Blog &apos;em Ipsum</h1>
+        <Posts />
+      </div>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
 
