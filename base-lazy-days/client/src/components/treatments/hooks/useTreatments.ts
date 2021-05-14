036618608013a72878dac59992500ed46a51eb4a
Bonnie Schulkin <biz@bonnieschulkin.com>
2021-05-13 18:16:50 -0700

    code useTreatments

--- base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -1,14 +1,18 @@
+import { useQuery } from 'react-query';
+
 import type { Treatment } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
 import { queryKeys } from '../../../react-query/constants';
 
-// for when we need a query function for useQuery
-// async function getTreatments(): Promise<Treatment[]> {
-//   const { data } = await axiosInstance.get('/treatments');
-//   return data;
-// }
+async function getTreatments(): Promise<Treatment[]> {
+  const { data } = await axiosInstance.get('/treatments');
+  return data;
+}
 
 export function useTreatments(): Treatment[] {
-  // TODO: get data from server via useQuery
-  return [];
+  const fallback = [];
+  const { data } = useQuery(queryKeys.treatments, getTreatments, {
+    placeholderData: fallback,
+  });
+  return data;
 }
