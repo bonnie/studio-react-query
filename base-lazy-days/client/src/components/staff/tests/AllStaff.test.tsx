480c792d608de5609b8b910b1ebafed1e343faa3
Bonnie Schulkin <biz@bonnieschulkin.com>
2021-05-17 16:20:24 -0700

    staff query code quiz

--- base-lazy-days/client/src/components/staff/tests/AllStaff.test.tsx
+++ base-lazy-days/client/src/components/staff/tests/AllStaff.test.tsx
@@ -2,11 +2,16 @@ import { screen } from '@testing-library/react';
 
 // import { rest } from 'msw';
 // import { server } from '../../../mocks/server';
-// import { renderWithQueryClient } from '../../../test-utils';
+import { renderWithQueryClient } from '../../../test-utils';
 import { AllStaff } from '../AllStaff';
 
-test('renders response from query', () => {
-  // write test here
+test('renders response from query', async () => {
+  renderWithQueryClient(<AllStaff />);
+  const staffNames = await screen.findAllByRole('heading', {
+    name: /divya|sandra|michael|mateo/i,
+  });
+
+  expect(staffNames).toHaveLength(4);
 });
 
 test('handles query error', async () => {
