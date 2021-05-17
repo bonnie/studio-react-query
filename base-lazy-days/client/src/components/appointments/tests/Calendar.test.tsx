bbb3f7b801654319263023e8012e647c06c666d7
Bonnie Schulkin <biz@bonnieschulkin.com>
2021-05-17 16:22:55 -0700

    remove useUser mock

--- base-lazy-days/client/src/components/appointments/tests/Calendar.test.tsx
+++ base-lazy-days/client/src/components/appointments/tests/Calendar.test.tsx
@@ -5,12 +5,6 @@ import { screen } from '@testing-library/react';
 // import { renderWithQueryClient } from '../../../test-utils';
 import { Calendar } from '../Calendar';
 
-// mocking useUser to mimic a logged-in user
-// jest.mock('../../user/hooks/useUser', () => ({
-//   __esModule: true,
-//   useUser: () => ({ user: mockUser }),
-// }));
-
 test('Reserve appointment error', () => {
   // (re)set handler to return a 500 error for appointments
   // server.resetHandlers(
