9de667a7de338fe5053f51a678dd172cf291ca53
Bonnie Schulkin <biz@bonnieschulkin.com>
2021-05-17 17:36:40 -0700

    staff error test code quiz

--- base-lazy-days/client/src/components/staff/tests/AllStaff.test.tsx
+++ base-lazy-days/client/src/components/staff/tests/AllStaff.test.tsx
@@ -1,7 +1,7 @@
 import { screen } from '@testing-library/react';
+import { rest } from 'msw';
 
-// import { rest } from 'msw';
-// import { server } from '../../../mocks/server';
+import { server } from '../../../mocks/server';
 import { renderWithQueryClient } from '../../../test-utils';
 import { AllStaff } from '../AllStaff';
 
@@ -16,9 +16,15 @@ test('renders response from query', async () => {
 
 test('handles query error', async () => {
   // (re)set handler to return a 500 error for staff
-  // server.resetHandlers(
-  //   rest.get('http://localhost:3030/staff', (req, res, ctx) => {
-  //     return res(ctx.status(500));
-  //   }),
-  // );
+  server.resetHandlers(
+    rest.get('http://localhost:3030/staff', (req, res, ctx) => {
+      return res(ctx.status(500));
+    }),
+  );
+
+  renderWithQueryClient(<AllStaff />);
+
+  // check for the toast alert
+  const alertToast = await screen.findByRole('alert');
+  expect(alertToast).toHaveTextContent('Request failed with status code 500');
 });
