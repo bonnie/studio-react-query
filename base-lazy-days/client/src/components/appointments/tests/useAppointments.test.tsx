a3ff2f70273c922489656830c3e4116adbddf8b6
Bonnie Schulkin <biz@bonnieschulkin.com>
2021-05-17 22:03:03 -0700

    update name of imported function from test-utils

--- base-lazy-days/client/src/components/appointments/tests/useAppointments.test.tsx
+++ base-lazy-days/client/src/components/appointments/tests/useAppointments.test.tsx
@@ -1,6 +1,6 @@
 // import { act, renderHook } from '@testing-library/react-hooks';
 
-// import { createWrapper } from '../../../test-utils';
+// import { createQueryClientWrapper } from '../../../test-utils';
 import { useAppointments } from '../hooks/useAppointments';
 
 test('filter appointments by availability', async () => {
